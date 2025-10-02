7a73f682bd53b932841b3df80c3209b35f2bb518
Able <abl3theabove@gmail.com>
2025-10-02 09:23:52 -0500

    merge :3

--- server/assets/entity_script.lua
+++ server/assets/entity_script.lua
@@ -12,7 +12,8 @@ function on_puppet(player_name)
  print("Puppeted by " .. player_name)
 end
 
-function on_unpuppet()
+function on_unpuppet(player_name)
+ print("Unpuppeted by " .. player_name)
 end
 
 
